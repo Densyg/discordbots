09376a1a12797de5c4de9fb7e3417656cc68390b
Naomi Que <naomi.que@student.unsw.edu.au>
2017-12-16 22:53:37 +1100

    bombs and birthdays and songs

--- ralph/bot.js
+++ ralph/bot.js
@@ -35,7 +35,8 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 
         var args = message.substring(1).split(' ');
         var cmd = args[0];
-
+        var arg1 = args[1];
+        var arg2 = args[2];
        
 
         args = args.splice(1);
@@ -43,22 +44,49 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 
             // !ping
             case 'ralph':
+                arg1 = String(arg1);
 
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'arf'
-                });
+                if (arg1 === 'bomb') {
+                    var max = 10;
+                    var handsflag = false;
+
+                    // check if next argument is a number
+                    if (!isNaN(arg2)) {
+                        max = arg2;
+                        if (max > 10) {
+                            max = 10;
+                            handsflag = true;
+                        }
+                    } 
+                   
+                    for (var i = 0; i < max; i++) {
+                        bot.sendMessage({
+                            to: channelID,
+                            message: 'https://imgur.com/gallery/Pts5d'
+                        });                        
+                    }
+
+                    if (handsflag) {
+                        bot.sendMessage({
+                            to: channelID,
+                            message: 'Ralph only have two hands'
+                        });                        
+                    }
+                } else {
+
+                    bot.sendMessage({
+                        to: channelID,
+                        message: 'arf'
+                    });
+                }
+
 
             break;
 
-            //case 'ralphbomb'
-            // Just add any case commands if you want to..
-            //    for i in 10
             
             case 'birthday':
-                var name = args[1];
-                name.toLowerCase();
-                if (name == 'weilon' || name == 'whale') {
+                var name = String(arg1);
+                if (name === 'weilon' || name === 'Weilon' || name === 'whale') {
                     bot.sendMessage({
                         to: channelID,
                         message: 'https://i.pinimg.com/originals/58/a9/41/58a941c56e9cf84178c34f05622409fc.jpg' // Nice picture of a whale saying "Happy Birthday"
@@ -66,9 +94,34 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 } else {
                     bot.sendMessage({
                         to: channelID,
-                        message: 'It whale birthday. Not yours. Sniffling dimwit.'
+                        message: 'It whale birthday. Not yours. *Sniffling dimwit*'
                     });
                 }
+                
+            break;
+
+            case 'sing':
+               bot.sendMessage({
+                   to: channelID,
+                   message: '*happy birthday to you*'
+               });
+
+               bot.sendMessage({
+                   to: channelID,
+                   message: '*you smell like a poo*'
+               });
+
+               bot.sendMessage({
+                   to: channelID,
+                   message: '*you look like purple scum*'
+               });
+
+               bot.sendMessage({
+                   to: channelID,
+                   message: '*\*spits\**'
+               });
+            break;
+
          }
 
      }
